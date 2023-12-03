7cddd15483605bd70a6fb9f9553da5f431edcbd4
CODERSOUV <deysouvagya723@gmail.com>
2023-12-03 09:38:07 +0530

    new

--- gui1.py
+++ gui1.py
@@ -1,11 +1,44 @@
-from tkinter import *
-root1=Tk()
-root1.geometry("600x200")
-root1.title("First gui")
-root2=Label(text='''Modern electronic calculators vary from cheap, give-away, \ncredit-card-sized models to sturdy desktop models with built-in printers. They \nbecame popular in the mid-1970s as the incorporation of integrated circuits \nreduced their size and cost. By the end of that decade, prices had dropped to \nthe point where a basic calculator was affordable to most and they became \ncommon in schools.''',bg="blue",fg="yellow",padx="23",pady="34",font=("comicsansms",16,"bold"),relief=SUNKEN
- )
-root2.pack(side=LEFT,anchor="nw",fill=Y,padx=34,pady=45)
-root1.mainloop()
-# padx - x padding
-# pady - y padding
-# relief - border styling - SUNKEN, RAISED, GROOVE, RIDGE
+import tkinter
+import turtle
+import tkinter.messagebox
+
+window = tkinter.Tk()
+
+canvas = tkinter.Canvas(master = window, width = 800, height = 800)
+canvas.grid(padx=2, pady=2, row=0, column=0, rowspan=10, columnspan=10)
+draw = turtle.RawTurtle(canvas)
+
+def Board(a, x, y, size):
+    #draw.pu()
+    draw.penup()
+    draw.goto(x,y)
+    #draw.pd()
+    draw.pendown()
+    for i in range (0, 4):
+        draw.forward(size)
+        draw.right(90)
+
+def Board2():
+    x =-40
+    y = -40
+    size = 40
+    for i in range (0, 10):
+        for j in range (0, 10):
+            Board(draw, x + j*size, y + i*size, size)
+
+def Button_click ():
+    tkinter.messagebox.showinfo("Game", "Tic Tac Toe")
+
+#button = tkinter.Button(window, text = "Play!", command = Button_click)
+#button = Tk.Button(window, text = "Play!", command = Button_click)
+#button.pack()
+#
+Play_Button = tkinter.Button(master = window, text ="Play!", command = Button_click)
+Play_Button.config(bg="cyan",fg="black")
+Play_Button.grid(padx=2, pady=2, row=0, column=11, sticky='nsew')
+
+Board_Button = tkinter.Button(master = window, text ="Draw_Board", command = Board2)
+Board_Button.config(bg="cyan",fg="black")
+Board_Button.grid(padx=2, pady=2, row=1, column=11, sticky='nsew')
+#
+window.mainloop()
